ff911426552ea87f2516369b9606acac12b618e2
shubham-alt <58552516+shubham-alt@users.noreply.github.com>
2024-12-19 19:22:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ pre_assigned_players = [
     {"Name": "KL Rahul", "Role": "WK", "Rating": 95, "Selling Price": 13, "Team": "Maverick"},
     {"Name": "Babar Azam", "Role": "BAT", "Rating": 90, "Selling Price": 8.5, "Team": "Goku"},
     {"Name": "Van der Dussen", "Role": "BAT", "Rating": 87, "Selling Price": 3, "Team": "Mospher"},
-    {"Name": "Pat Cummins", "Role": "BOWL", "Rating": 89, "Selling Price": 9.5, "Team": "Maverick"},
+    {"Name": "Pat Cummins", "Role": "BOWL", "Rating": 89, "Selling Price": 9, "Team": "Maverick"},
     {"Name": "Saud Shakeel", "Role": "BAT", "Rating": 86, "Selling Price": 6.5, "Team": "Maverick"},
     {"Name": "Tim Southee", "Role": "BOWL", "Rating": 82, "Selling Price": 3, "Team": "Mospher"}    
 ]
